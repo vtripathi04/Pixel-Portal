882c490432c1712e8c56bdc9eb625a88fc5ef659
Vishwajeet Tripathi <tripathi.vishwajeet12345@gmail.com>
2024-01-24 17:41:45 +0530

    add auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,46 @@
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/users.model.js";
+import { jwt } from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+const verifyJWT = asyncHandler(async (req, _, next) => {
+    
+    // extract token from cookies or headers
+    // verify the token using jwt.verify
+    // Search for user with _id in payload
+    // assign user doc as req property user
+
+    try {
+        
+        const token = req.cookies?.accessToken || req.header("Authorization").replace("Bearer ", "")
+
+        if(!token){
+
+            throw new ApiError(401, "Unauthorized Request")
+        }
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        
+
+        const user = await User.findById(decoded?._id).select("-password -refreshToken")
+
+        if (!user) {
+            
+            throw new ApiError(401, "Invalid Access Token")
+        }
+
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        
+        throw new ApiError(401, error?.message || "Invalid access token")
+
+    }
+
+
+})
+
+export { verifyJWT }
